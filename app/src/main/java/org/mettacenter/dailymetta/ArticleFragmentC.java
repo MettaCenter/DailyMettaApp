b2a5c9223b8e18d5362ce287a5c7b3a196fa9ad8
Tord <tord.dellsen@gmail.com>
2015-07-16 00:51:16 +0200

    Fixing the links inside the article html

--- app/src/main/java/org/mettacenter/dailymetta/ArticleFragmentC.java
+++ app/src/main/java/org/mettacenter/dailymetta/ArticleFragmentC.java
@@ -7,6 +7,8 @@ import android.provider.BaseColumns;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,14 +33,21 @@ public class ArticleFragmentC extends Fragment {
 
         Bundle tArgs = getArguments();
 
-        TextView tArticleTv = ((TextView) rRootView.findViewById(android.R.id.text1));
+        TextView tArticleTv = ((TextView) rRootView.findViewById(R.id.article));
         android.text.Spanned tArticleHtmlFormatted = Html.fromHtml(tArgs.getString(ARG_ARTICLE)
                 .replaceAll("<img.+/(img)*>", ""));
         tArticleTv.setText(tArticleHtmlFormatted); // + tArticleText
 
+        /*
+        Linking to the web. Please note that we need to avoid using android:autoLink="web" here
+        which is for plain text links and not links inside <a> tags
+         */
+        tArticleTv.setMovementMethod(LinkMovementMethod.getInstance());
+
         TextView tLinkTv = ((TextView) rRootView.findViewById(R.id.link_to_article));
         android.text.Spanned tLinkHtmlFormatted = Html.fromHtml(tArgs.getString(ARG_LINK));
         tLinkTv.setText(tLinkHtmlFormatted);
+        tLinkTv.setMovementMethod(LinkMovementMethod.getInstance());
 
         return rRootView;
     }
