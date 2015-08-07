e2bc2c62bd2456a478cf96503de4f225f07c66b3
Tord <tord.dellsen@gmail.com>
2015-08-07 20:16:52 +0200

    Robotium added * dependency added to build.gradle file * First Robotium test case added (and successfully run): RobotiumExTest.java (The app.iml file it seems was automatically updated)

---
+++ app/src/androidTest/java/org/mettacenter/dailymettaapp/RobotiumExTest.java
@@ -0,0 +1,54 @@
+package org.mettacenter.dailymettaapp;
+
+/**
+ * Created by sunyata on 2015-08-07.
+ */
+import android.test.ActivityInstrumentationTestCase2;
+import com.robotium.solo.Solo;
+
+@SuppressWarnings("rawtypes")
+public class RobotiumExTest extends ActivityInstrumentationTestCase2 {
+    private Solo mRobotium;
+
+    private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "org.mettacenter.dailymettaapp.ArticleActivityC";
+    private static final String LAUNCHER_ACTIVITY_PARTIAL_CLASSNAME = "ArticleActivityC";
+
+    private static Class<?> launcherActivityClass;
+    static{
+        try {
+            launcherActivityClass = Class.forName(LAUNCHER_ACTIVITY_FULL_CLASSNAME);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public RobotiumExTest() throws ClassNotFoundException {
+        super(launcherActivityClass);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        mRobotium = new Solo(getInstrumentation());
+        getActivity();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        mRobotium.finishOpenedActivities();
+        super.tearDown();
+    }
+
+    public void testRun() {
+        //Wait for activity: 'com.example.ExampleActivty'
+        mRobotium.waitForActivity(LAUNCHER_ACTIVITY_PARTIAL_CLASSNAME, 2000);
+
+        mRobotium.scrollDown();
+
+        /*
+        //Clear the EditText editText1
+        mRobotium.clearEditText((android.widget.EditText) mRobotium.getView("editText1"));
+        mRobotium.enterText((android.widget.EditText) mRobotium.getView("editText1"), "This is an example text");
+        */
+    }
+}
